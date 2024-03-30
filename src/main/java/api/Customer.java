599db2fe08888652c2ee53f76685b7f2e3412864
admin <krzysiek.chrzan@gmail.com>
2024-03-31 00:32:45 +0100

    verification if customer has been added

--- src/main/java/api/Customer.java
+++ src/main/java/api/Customer.java
@@ -1,6 +1,7 @@
 package api;
 
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,6 +12,8 @@ import io.restassured.specification.RequestSpecification;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.json.JSONException;
@@ -21,38 +24,48 @@ public class Customer {
     Properties properties = new Properties();
     private String url = null;
     private Response response = null;
+    private String customerId = null;
 
     @Before(value = "@API")
     public void setupApi() throws IOException {
-        properties = new Properties();
-        properties.load(new FileReader("src\\main\\resources\\config.properties"));
+        properties = new Properties();properties.load(new FileReader("src\\main\\resources\\config.properties"));
         url = properties.getProperty("REST_API_URL") + "customer";
     }
 
 
     @When("I make a POST request to customer")
-    public void requestPostToCustomer() {
-        // create a request object
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
-        request.header("CDate", "simpleDate");
+    public void requestPostToCustomer(DataTable datatable) {
+        List<Map<String,String>> lista = datatable.asMaps(String.class,String.class);
+        Map<String, String> map = lista.get(0);
+        String name = map.get("Name").toString();
+        String surname = map.get("Surname").toString();
+        String age = map.get("Age").toString();
+//        for(Map m : lista){
+//            name = m.get("Name").toString();
+//            surname = m.get("Surname").toString();
+//        }
 
-        // create a request body
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.header("Content-Type", "application/json");
+        requestSpecification.header("CDate", "simpleDate");
+
+        // create a body
         JSONObject requestJSON = new JSONObject();
         try {
             requestJSON
-                    .put("name", "Krzysztof")
-                    .put("surname", "Chrzan")
-                    .put("age", "40");
+                    .put("name", name)
+                    .put("surname", surname)
+                    .put("age", age);
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
-        request.body(requestJSON.toString());
+        requestSpecification.body(requestJSON.toString());
 
         // send a request
-        response = request.post(url);
-        System.out.println(response.body().asString());
+        response = requestSpecification.post(url);
 
+        customerId = response.jsonPath().get("id").toString();
+        System.out.println(customerId);
     }
 
     @Then("I verify that response has status {string}")
@@ -67,21 +80,17 @@ public class Customer {
         // create a request object
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
-        request.header("CDate", "simpleDate");
 
-        response = request.get(url +  "?surname=Chrzan");
-        System.out.println(response.toString());
+        response = request.get(url + "?id=" + customerId);
+        System.out.println(response.jsonPath().get("[0]").toString());
+        Assert.assertEquals(response.jsonPath().get("id[0]").toString(), customerId );
+        Assert.assertEquals(response.jsonPath().get("name[0]").toString(), "Jan" );
+        Assert.assertEquals(response.jsonPath().get("surname[0]").toString(), "Kovalski" );
     }
 
-    @Then("I verify that response has customer")
-    public void verifyGetResponse() {
-        String s = response.jsonPath().get("x[0].surname").toString();
-        System.out.println(s);
-//        JSONObject json = new JSONObject();
-//        json = response.toString();
-//        System.out.println("The status received: " + response.statusLine());
-//        Assert.assertEquals(response.statusCode(), statusInt);
-    }
+    @When("I make a GET request for all customers")
+    public void getAllCustomers() {
 
+    }
 
 }
--- src/main/resources/features/add_new_customer.feature
+++ src/main/resources/features/add_new_customer.feature
@@ -4,10 +4,10 @@ Feature: Add new customer to the bookstore
   @API
   Scenario: Create a new customer
     When I make a POST request to customer
+      | Name | Surname  | Age |
+      | Jan  | Kovalski | 41  |
     Then I verify that response has status "201"
-
-  @API
-  Scenario: Get information about existing customer
     When I make a GET request to customer
     Then I verify that response has status "200"
-    And I verify that response has customer
+    Then I make a GET request for all customers
+
